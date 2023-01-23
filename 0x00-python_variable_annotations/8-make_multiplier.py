5e93926e81e0609e4116c4b1d4080471827d6175
fernan17893 <fernan17893@gmail.com>
2023-01-23 15:55:42 -0400

    all tasks

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Complex types - functions """
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """ type-annotated function  """
+    def fn(n: float):
+        return n * multiplier
+    return 
