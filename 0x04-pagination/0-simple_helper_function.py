6c571f683de9c137e5a15bcf40cd749ac6d56e1c
fernan17893 <fernan17893@gmail.com>
2023-02-06 17:00:08 -0400

    all tasks pagination

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def index_range(page, page_size):
+    """Return a tuple of size two containing a start index and an end index
+       corresponding to the range of indexes to return in a list for those
+       particular pagination parameters.
+
+       page: current page number
+       page_size: number of items per page
+       Return: a tuple of size two
+    """
+    return ((page - 1) * page_size, page * page_size)
