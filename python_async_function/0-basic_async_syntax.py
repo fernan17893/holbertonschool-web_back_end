16672bae459fdd61be76f497c1a24b423764671e
fernan17893 <fernan17893@gmail.com>
2023-05-19 10:55:55 -0400

    task 0 try

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Async task 0 """
+
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) ->float:
+    """Asynchronus coroutine waits for delay"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
