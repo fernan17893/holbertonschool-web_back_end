c62301111f8cd199104fa5d2fbc7be362c1927df
fernan17893 <fernan17893@gmail.com>
2023-05-19 12:40:26 -0400

    this should work but it doesnt

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 """ Async task 2 """
 
-import asyncio
+from asyncio import run
 import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
-async def measure_time(n: int = 0, max_delay:int = 10) -> float:
+async def measure_time(n: int, max_delay:int) -> float:
     """Async routine that measures execution time for wait_n"""
-    start = time.perf_counter()
-    asyncio.run(wait_n(n, max_delay))
-    end = time.perf_counter()
-
+    start = time()
+    run(wait_n(n, max_delay))
+    end = time()
     total_time = end - start
     return total_time / n
