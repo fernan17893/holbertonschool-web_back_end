10363ff8bc23a7330aa946cd81052982411a9039
fernan17893 <fernan17893@gmail.com>
2023-05-22 15:18:34 -0400

    1st task try

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""BasicCache module"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """BasicCache class"""
+
+    def put(self, key, item):
+        if key or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None:
+            return None
+
+        if key not in self.cache_data:
+            return None
+
+        return self.cache_data.get(key)
