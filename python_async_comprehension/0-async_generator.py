08289e8b4825973ff10e7469a4f01a73c1d58f8b
fernan17893 <fernan17893@gmail.com>
2023-05-22 12:49:00 -0400

    anotate fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Async Generator"""
     for i in range(10):
         await asyncio.sleep(1)
