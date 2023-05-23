fe5d7d7c620b35ab3a4ea7f58d2f92a755a5ce95
fernan17893 <fernan17893@gmail.com>
2023-05-23 14:06:15 -0400

    task 4 done and adv try

--- caching/100-lfu_cache.py
+++ caching/100-lfu_cache.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python3
-"""MRU Caching"""
+"""LFU Caching"""
 
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class MRUCache(BaseCaching):
-    """MRU Cache Class"""
+class LFUCache(BaseCaching):
+    """LFU Cache Class"""
     def __init__(self):
         super().__init__()
         self.keys = []
+        self.count = {}
 
     def put(self, key, item):
-        """Returns the value most recently used"""
+        """Returns the value least frequently used"""
         if key is None or item is None:
             return
 
         self.cache_data[key] = item
         if key not in self.keys:
             self.keys.append(key)
+            self.count[key] = 1
+        else:
+            self.count[key] += 1
 
         if len(self.keys) > BaseCaching.MAX_ITEMS:
-            last = self.keys.pop(-2)
-            del self.cache_data[last]
-            print("DISCARD:", last)
+            min = self.count[self.keys[0]]
+            for k in self.keys:
+                if self.count[k] < min:
+                    min = self.count[k]
+            for k in self.keys:
+                if self.count[k] == min:
+                    del self.cache_data[k]
+                    self.keys.remove(k)
+                    del self.count[k]
+                    print("DISCARD:", k)
+                    break
 
     def get(self, key):
         """Returns the value in the dictionary `cache_data`
@@ -36,4 +48,5 @@ class MRUCache(BaseCaching):
         if key not in self.cache_data:
             return None
 
+        self.count[key] += 1
         return self.cache_data[key]
