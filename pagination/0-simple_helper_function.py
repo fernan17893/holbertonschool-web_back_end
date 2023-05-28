4b24527cc662ad53eebbd968a923140543874258
fernan17893 <fernan17893@gmail.com>
2023-05-27 21:34:38 -0400

    full push

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""task 0"""
+
+def index_range(page, page_size) -> tuple:
+    """Return a tuple of size two containing a start index and an end index
+       corresponding to the range of indexes to return in a list for those
+       particular pagination parameters.
+
+       page: current page number
+       page_size: number of items per page
+       Return: a tuple of size two
+    """
+    return ((page - 1) * page_size, page * page_size)
