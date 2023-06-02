d867fce54d21812f719588290cd1071e5725c940
fernan17893 <fernan17893@gmail.com>
2023-06-02 11:00:50 -0400

    pycode fix

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -9,19 +9,17 @@ from typing import List, TypeVar
 class Auth:
     """Auth class"""
 
-
     def __init__(self):
         """Constructor"""
-
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Requires authorization method"""
         return False
-    
+
     def authorization_header(sefl, request=None) -> str:
         """Authorization header method"""
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """Current user method"""
         return None
