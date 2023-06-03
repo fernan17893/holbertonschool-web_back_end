21540c4febf73a1246136d99b20cf679ae5f10f5
fernan17893 <fernan17893@gmail.com>
2023-06-03 11:30:07 -0400

    task

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,20 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Requires authorization method"""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] == '*':
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif path == excluded_path:
+                return False
+        return True
+        
 
     def authorization_header(sefl, request=None) -> str:
         """Authorization header method"""
