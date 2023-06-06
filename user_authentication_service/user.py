e868ae978973cc0fe830df31a6154ff9f89447ee
fernan17893 <fernan17893@gmail.com>
2023-06-06 16:52:57 -0400

    first task push/try

---
+++ user_authentication_service/user.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""User class module"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), unique=True, nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        """Return string representation of User"""
+        return "<User(id='%s', email='%s', hashed_password='%s', \
+session_id='%s', reset_token='%s')>" % (
+            self.id, self.email, self.hashed_password,
+            self.session_id, self.reset_token)
