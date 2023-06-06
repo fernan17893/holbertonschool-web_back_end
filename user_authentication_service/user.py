4f777ccacc0150a3322b32b57073a6f43b2c0158
fernan17893 <fernan17893@gmail.com>
2023-06-06 19:10:11 -0400

    0 and 1 fixed

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -9,7 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class User(Base) :
+class User(Base):
     """User class"""
     __tablename__ = 'users'
 
@@ -18,10 +18,3 @@ class User(Base) :
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-    def __repr__(self):
-        """Return string representation of User"""
-        return "<User(id='%s', email='%s', hashed_password='%s', \
-session_id='%s', reset_token='%s')>" % (
-            self.id, self.email, self.hashed_password,
-            self.session_id, self.reset_token)
