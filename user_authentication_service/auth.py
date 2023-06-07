7cc77196332906bdea83e548f32a319cf0cd8024
fernan17893 <fernan17893@gmail.com>
2023-06-07 10:26:05 -0400

    uuid

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -6,6 +6,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -36,3 +37,7 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
+        
+    def _generate_uuid(self) -> str:
+        """Generate UUIDs"""
+        return str(uuid.uuid4())
