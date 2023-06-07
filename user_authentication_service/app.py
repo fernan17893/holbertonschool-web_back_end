8ec7ac8b31c82960c1a9cf0d5e6beef94ac9294c
fernan17893 <fernan17893@gmail.com>
2023-06-06 21:18:23 -0400

    task 7 try

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,14 +2,31 @@
 """Basic Flask APP"""
 
 
-from flask import Flask, jsonify, request, abort, redirect
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+AUTH = Auth()
+
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def index() -> str:
+@app.route('/', methods=['GET'])
+def get_request() -> str:
     """Get Flask JSON payload"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users() -> str:
+    """Register user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
