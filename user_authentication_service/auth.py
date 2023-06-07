59e0c579bb5a0e2ccc15e842c0b7be5c4b76a81d
fernan17893 <fernan17893@gmail.com>
2023-06-06 20:46:41 -0400

    5 done

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,8 +3,27 @@
 
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """Takes in a password string arguments and returns bytes"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """register new user"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
