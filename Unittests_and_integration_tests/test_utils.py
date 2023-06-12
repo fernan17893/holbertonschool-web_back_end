a7176cee17ea089de236043a7a1067a47cd79650
fernan17893 <fernan17893@gmail.com>
2023-06-12 16:40:31 -0400

    task 1 try no pycode

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,11 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Test access nested map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), 1),
+        ({"a": 1}, ("a", "b") , 2),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """Test access nested map exception"""
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
