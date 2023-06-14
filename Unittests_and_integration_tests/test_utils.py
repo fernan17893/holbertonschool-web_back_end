c286913cd5b4419dff0e7fd6cebfe0284e582bd4
fernan17893 <fernan17893@gmail.com>
2023-06-14 11:39:31 -0400

    ll

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -29,15 +29,22 @@ class TestAccessNestedMap(unittest.TestCase):
 
 
 class TestGetJson(unittest.TestCase):
-    """TestGetJson class"""
-
+    """
+    test get json
+    """
     @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False}),
+        ("http://example.com", {"test_payload": True}),
+        ("http://holberton.io", {"test_payload": False})
     ])
     def test_get_json(self, test_url, test_payload):
-        """Test get json"""
-        with patch('requests.get') as mock_get:
-            mock_get.return_value.json.return_value = test_payload
-            self.assertEqual(get_json(test_url), test_payload)
-            mock_get.assert_called_once_with(test_url)
+        """
+            patch the requests.get
+            to return a fake payload
+            assert that we're getting
+            the expected result
+        """
+        with patch('requests.get') as mc:
+            mc.return_value.json.return_value = test_payload
+            response = get_json(test_url)
+            self.assertEqual(response, test_payload)
+            mc.assert_called_once()
