dc75ad0ff058f9a317b64fb91afb665caac7ab13
fernan17893 <fernan17893@gmail.com>
2023-06-14 11:17:03 -0400

    checker decorator

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -31,11 +31,11 @@ class TestAccessNestedMap(unittest.TestCase):
 class TestGetJson(unittest.TestCase):
     """Test get json class"""
 
-    @patch('requests.get')
     @parameterized.expand([
         ("http://example.com", {"test_payload": True}),
         ("http://holberton.io", {"test_payload": False})
     ])
+    @patch('requests.get')
     def test_get_json(self, mock_get, test_url, test_payload):
         """Test get json"""
 
