feda1fa8227ef4f1886812669cb7e8473a2a1934
fernan17893 <fernan17893@gmail.com>
2023-07-04 14:39:21 -0400

    pycode fix

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -4,7 +4,6 @@
 from pymongo import MongoClient
 
 
-
 def list_all(mongo_collection):
     """ lists all documents in a collection """
     if mongo_collection is None or mongo_collection.find() is None:
