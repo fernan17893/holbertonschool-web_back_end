6991bee43a21680c7da02816765438e87a5c327a
fernan17893 <fernan17893@gmail.com>
2023-07-14 11:56:13 -0400

    task 1 try

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,15 +1,15 @@
 export default function getFullResponseFromAPI(success) {
-    const promise = new Promise((resolve, reject) => {
-      if (success) {
-        const response = {
-          status: 200,
-          body: 'Success',
-        };
-        resolve(response);
-      } else {
-        const error = new Error('The fake API is not working currently');
-        reject(error);
-      }
-    });
-    return promise;
-  }
+  const promise = new Promise((resolve, reject) => {
+    if (success) {
+      const response = {
+        status: 200,
+        body: 'Success',
+      };
+      resolve(response);
+    } else {
+      const error = new Error('The fake API is not working currently');
+      reject(error);
+    }
+  });
+  return promise;
+}
