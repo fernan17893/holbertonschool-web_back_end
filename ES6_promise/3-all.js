df4b774f48ff40b2f3ec78234e6587ed661502e9
fernan17893 <fernan17893@gmail.com>
2023-07-14 12:15:45 -0400

    2 done 3 try

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-duplicates */
+import uploadPhoto from './utils';
+import createUser from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()]).then((values) => {
+    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+  }).catch(() => console.log('Signup system offline'));
+}
