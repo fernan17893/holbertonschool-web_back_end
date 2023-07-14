65b84de0b8c2e936334bf8d04ce4bf63ef8596e3
fernan17893 <fernan17893@gmail.com>
2023-07-14 12:24:02 -0400

    task 3 fix

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,7 @@
-/* eslint-disable import/no-duplicates */
-import uploadPhoto from './utils';
-import createUser from './utils';
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then((values) => {
     console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
-  }).catch(() => console.log('Signup system offline'));
+  }).catch(() => Error()).finally(() => console.log('Got a response from the API'));
 }
