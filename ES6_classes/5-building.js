cab0b6715b083f0334ba4d8cb3ec1a73f06503c8
fernan17893 <fernan17893@gmail.com>
2023-07-17 14:52:17 -0400

    task 5

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,23 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      this.evacuationWarningMessage();
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
