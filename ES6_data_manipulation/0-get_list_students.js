dec29e76c0c464e92fde4f3934db6c8dc8c93bec
fernan17893 <fernan17893@gmail.com>
2023-07-19 14:28:43 -0400

    linted fix

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,16 @@
 export default function getListStudents(id, firstName, location) {
-    return [
-        { id: 1, firstName: 'Guillame', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco'}
-    ]
+  if (typeof id !== 'number') {
+    throw new TypeError('id must be a number');
+  }
+  if (typeof firstName !== 'string') {
+    throw new TypeError('firstName must be a string');
+  }
+  if (typeof location !== 'string') {
+    throw new TypeError('location must be a string');
+  }
+  return [
+    { id: 1, firstName: 'Guillame', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco'}
+  ];
 }
