19570038f1ea460165744e9d222f1b4524143328
fernan17893 <fernan17893@gmail.com>
2023-07-19 14:38:14 -0400

    lint fix and implement fix

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,16 +1,7 @@
-export default function getListStudents(id, firstName, location) {
-  if (typeof id !== 'number') {
-    throw new TypeError('id must be a number');
-  }
-  if (typeof firstName !== 'string') {
-    throw new TypeError('firstName must be a string');
-  }
-  if (typeof location !== 'string') {
-    throw new TypeError('location must be a string');
-  }
+export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillame', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco'}
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
