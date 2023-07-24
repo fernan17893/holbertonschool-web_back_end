902c8062b2f50c93630dc2d866b9b10bdd657abf
fernan17893 <fernan17893@gmail.com>
2023-07-24 12:16:50 -0400

    lint fix

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,5 +2,5 @@ export default function getStudentsByLocation(array, city) {
   if (!Array.isArray(array)) {
     return [];
   }
-  return array.filter((item) => (item.location === city));
+  return array.filter((item) => item.location === city);
 }
