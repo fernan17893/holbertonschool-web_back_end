ee739a9390e3a365bcd76f875c88cbc2525739e7
fernan17893 <fernan17893@gmail.com>
2023-07-25 11:46:57 -0400

    task 2 newline and code fix

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -4,15 +4,14 @@ function countStudents(path) {
   try {
     const content = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' }).split('\n');
     let total = content.length - 1;
-    content.forEach((item) => {
+    for (const item of content) {
       if (item.length === 0) {
         total -= 1;
       }
-      console.log(item.leng);
-    });
+    }
     console.log(`Number of students: ${total}`);
-  } catch (err) {
-    throw console.error('Cannot load the database');
+  } catch (e) {
+    throw new Error('Cannot load the database');
   }
 }
 
