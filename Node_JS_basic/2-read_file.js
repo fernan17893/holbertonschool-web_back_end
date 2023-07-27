5b8eef2c84acfbcbcdacb6a02d2254379bc072c7
fernan17893 <fernan17893@gmail.com>
2023-07-27 19:02:46 -0400

    big fix task 2

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -10,6 +10,17 @@ function countStudents(path) {
       }
     }
     console.log(`Number of students: ${total}`);
+    const csStudents = [];
+    const sweStudents = [];
+    for (const item of content) {
+      if (item.includes('CS')) {
+        csStudents.push(item.split(',')[0]);
+      } else if (item.includes('SWE')) {
+        sweStudents.push(item.split(',')[0]);
+      }
+    }
+    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
   } catch (e) {
     throw new Error('Cannot load the database');
   }
