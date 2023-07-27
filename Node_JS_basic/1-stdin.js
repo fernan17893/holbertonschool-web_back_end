5dadaa48f74407508c8e02732c658e09c9f300d5
fernan17893 <fernan17893@gmail.com>
2023-07-27 18:45:54 -0400

    fix try

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -9,6 +9,6 @@ const prompt = 'Welcome to Holberton School, what is your name?\n';
 
 rl.question(prompt, (name) => {
   console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing.');
-  rl.close();
+  console.log('This important software is now closing');
+  process.exit();
 });
