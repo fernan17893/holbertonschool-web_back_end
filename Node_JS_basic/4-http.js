b623cc29162cc526b676045bfbf9d092b84382ba
fernan17893 <fernan17893@gmail.com>
2023-07-28 13:05:12 -0400

    4 listen and fix

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -5,6 +5,7 @@ const port = 1245;
 
 const requestListner = function(request, response) {
   response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
   response.end('Hello Holberton School!');
 };
 
@@ -12,3 +13,7 @@ const server = http.createServer(requestListner);
 server.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}/`);
 });
+
+server.listen(1245);
+
+module.exports = server;
