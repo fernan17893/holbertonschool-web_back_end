de604763c3a7380079a7e908e7417d087f20ccdf
fernan17893 <fernan17893@gmail.com>
2023-07-28 13:32:42 -0400

    did it my way task 5 switch squad

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const url = require('url');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  const urlPath = url.parse(req.url, true);
+  console.log(req.url, urlPath);
+
+  switch (urlPath.pathname) {
+    case '/':
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Hello Holberton School!');
+      break;
+    case '/students':
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.write('This is the list of our students\n');
+      countStudents(process.argv[2])
+        .then((data) => res.end(data))
+        .catch((error) => res.end(error.message));
+      break;
+    default:
+      break;
+  }
+});
+app.listen(1245);
+
+module.exports = app;
