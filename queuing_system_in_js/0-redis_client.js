2bbaf50c9c4d6d6c2b1118faa3f7e2775973a049
fernan17893 <fernan17893@gmail.com>
2023-08-01 14:37:12 -0400

    task 1 or 0

---
+++ queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,11 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on("connect", () => {
+    console.log("Redis client connected");
+});
+
+client.on("error", (error) => {
+    console.log(`Redis client not connected to the server: ${error}`);
+});
