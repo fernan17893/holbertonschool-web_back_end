7d5d8743cd0923b8578cf90d5b4a106bc337c526
fernan17893 <fernan17893@gmail.com>
2023-08-05 12:36:24 -0400

    last push

---
+++ queuing_system_in_js/8-job.js
@@ -0,0 +1,13 @@
+export default function creatPushNotificationsJobs(jobs, queue) {
+    if (Array.isArray(jobs) === false) throw Error('Jobs is not an array');
+    
+    jobs.forEach(jobData => {
+        const job = queue.create('push_notification_code_3', jobData)
+            .save((err) => {
+                if (!err) console.log(`Notification job created: ${job.id}`);
+            });
+    job.on('complete', () => console.log(`Notification job ${job.id} completed`));
+    job.on('failed', (err) => console.log(`Notification job ${job.id} failed: ${err}`));
+    job.on('progress', (progress) => console.log(`Notification job ${job.id} ${progress}% complete`));
+        });
+}
