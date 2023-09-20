94082f4fe3129afc87551c4558b25c4108640ccd
fernan17893 <fernan17893@gmail.com>
2023-09-20 11:18:27 -0400

    3 fix and push 4-7

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,3 @@
 --script that lists glam rock bands
 SELECT band_name, COALESCE(split, 2020) - formed AS lifespan 
-FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
+FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC LIMIT NONE;
